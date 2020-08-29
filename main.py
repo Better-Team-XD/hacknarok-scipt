9546ad483e9405a3b385dec86a0e40c48944faba
krzysztof <misiek.nalepa@wp.pl>
2020-08-29 18:34:16 +0200

    recipe webscraping finished

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-URL = 'https://www.przepisy.pl/'
+URL = 'https://www.przepisy.pl/przepis/'
+
+
+def get_recipe(recipe_name_url):
+    current_url = URL + recipe_name_url
+    source = requests.get(current_url)
+    if source.ok:
+        soup = BeautifulSoup(source.text, 'html.parser')
+        div_list = soup.find_all('div', class_='ingredients-list-content-item')
+        name_list = []
+        for element in div_list:
+            name_list.append(element.find('p', class_='ingredient-name').text.strip().capitalize())
+        return name_list
+    raise Exception("Bad source exception")
+
+
+if __name__ == "__main__":
+    print(get_recipe('pomidorowa-z-makaronem'))
+
