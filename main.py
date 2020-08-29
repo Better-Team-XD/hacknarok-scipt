abef0cf906248e7f1013e4e3f114a993517d5749
krzysztof <misiek.nalepa@wp.pl>
2020-08-29 18:41:58 +0200

    name change

--- main.py
+++ main.py
@@ -4,19 +4,19 @@ import requests
 URL = 'https://www.przepisy.pl/przepis/'
 
 
-def get_recipe(recipe_name_url):
+def get_recipe_ingredients(recipe_name_url):
     current_url = URL + recipe_name_url
     source = requests.get(current_url)
     if source.ok:
         soup = BeautifulSoup(source.text, 'html.parser')
         div_list = soup.find_all('div', class_='ingredients-list-content-item')
-        name_list = []
+        ingredients_list = []
         for element in div_list:
-            name_list.append(element.find('p', class_='ingredient-name').text.strip().capitalize())
-        return name_list
+            ingredients_list.append(element.find('p', class_='ingredient-name').text.strip().capitalize())
+        return ingredients_list
     raise Exception("Bad source exception")
 
 
 if __name__ == "__main__":
-    print(get_recipe('pomidorowa-z-makaronem'))
+    print(get_recipe_ingredients('kruche-ciasto-z-budyniowa-pianka-i-owocami-12057'))
 
